096c25142fd415707a69a40c054699e7e6aa35c1
NataliyaYadykina <nataliya.yadykina@yandex.ru>
2023-10-28 08:59:05 -0900

    hw3.1 - numberInInterval method edited

--- src/homework/hw3/SomeServiceHW.java
+++ src/homework/hw3/SomeServiceHW.java
@@ -11,8 +11,8 @@ public class SomeServiceHW {
     // HW 3.2. Нужно написать метод, который проверяет,
     // попадает ли переданное число в интервал (25;100) и возвращает true, если попадает и false - если нет,
     // покрыть тестами метод на 100%
-    public boolean numberInInterval(int i) {
-
+    public boolean numberInInterval(int n) {
+        return n > 25 && n < 100;
     }
 
 }
